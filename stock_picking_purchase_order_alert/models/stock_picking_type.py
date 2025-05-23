f850afcb90c61db47fb0beb24ce037878586d262
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2025-05-22 20:45:59 -0400

    [ADD] stock_picking_order_alert

---
+++ stock_picking_purchase_order_alert/models/stock_picking_type.py
@@ -0,0 +1,24 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    display_quantity_alert_percentage = fields.Boolean(
+        string="Display Quantity Alert Percentage",
+        default=False,
+    )
+    quantity_alert_percentage = fields.Float(
+        string="Quantity Alert Percentage",
+        default=30.0,
+        help="Percentage threshold for quantity alert in receipts. "
+        "Default is 30%. Set to 0 to disable alerts.",
+    )
+
+    groups_ids = fields.Many2many(comodel_name="res.groups")
+
+    @api.constrains("quantity_alert_percentage")
+    def _check_quantity_alert_percentage(self):
+        if self.quantity_alert_percentage <= 0:
+            raise UserError(_("Quantity alert percentage must be greater than zero."))
