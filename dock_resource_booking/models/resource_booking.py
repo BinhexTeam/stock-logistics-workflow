2e3a4a3b38eb2ec97dfbc136995e4ccb8ec0df3c
r.perez <r.perez@binhex.cloud>
2025-08-21 16:39:10 -0400

    [IMP] dock_resource_booking: no auto creation of booking in picking creation or update, migrate onchanges to contrains

--- dock_resource_booking/models/resource_booking.py
+++ dock_resource_booking/models/resource_booking.py
@@ -6,24 +6,19 @@ class ResourceBooking(models.Model):
     _inherit = "resource.booking"
 
     picking_ids = fields.One2many(
-        "stock.picking", "booking_id", string="Associated Pickings"
+        "stock.picking",
+        "booking_id",
+        string="Associated Pickings",
+        domain="[('state', 'not in', ['done', 'cancel']), \
+              ('booking_id', '=', False), ('scheduled_date', '>=', start)]",
     )
 
-    @api.onchange("start")
-    def _onchange_date_start(self):
-        for booking in self:
-            for picking in booking.picking_ids:
-                picking.scheduled_date = booking.start
-
-    @api.constrains("resource_id", "start", "date_end")
-    def _check_booking_collision(self):
-        for booking in self:
-            domain = [
-                ("id", "!=", booking.id),
-                ("resource_id", "=", booking.resource_id.id),
-                ("start", "<", booking.date_end),
-                ("date_end", ">", booking.start),
-            ]
-            overlapping = self.search(domain)
-            if overlapping:
-                raise ValidationError(_("Dock is already reserved for this time slot."))
+    @api.constrains("start")
+    def _check_start(self):
+        for record in self.filtered("picking_ids"):
+            if record.start:
+                min_scheduled_date = min(record.picking_ids.mapped("scheduled_date"))
+                if min_scheduled_date < record.start:
+                    raise ValidationError(
+                        _("Pickings should be scheduled after booking start!")
+                    )
